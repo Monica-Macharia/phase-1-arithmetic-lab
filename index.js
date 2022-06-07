03023533956283b994d1a9d2da68b7c4c59d5d51
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-06-07 13:55:08 +0300

    commit tree e10adaa30099a967b3f14727730537529f602ad6

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 62;
+const num2 = 1;
+const multiply = num1  * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()* 1000) +1;
+
+const num3 = 22;
+const num4 = 18;
+const mod = num3 % num4;
+
+const values = (4,2,2,2,2,2,20,4,20);
+const max = Math.max(values);
+
